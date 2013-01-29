c4397734c935a52fdc8fb534d48594ecb8c3f2ca
Mark Allen <mark.allen3@student.dit.ie>
2013-01-29 13:44:00 +0000

    Using Oracle example code

--- src/clientServer/Server.java
+++ src/clientServer/Server.java
@@ -1,13 +1,47 @@
 package clientServer;
 
-public class Server {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	import java.net.*;
+	import java.io.*;
+	 
+	public class Server {
+	    public static void main(String[] args) throws IOException {
+	 
+	        ServerSocket serverSocket = null;
+	        try {
+	            serverSocket = new ServerSocket(4444);
+	        } catch (IOException e) {
+	            System.err.println("Could not listen on port: 4444.");
+	            System.exit(1);
+	        }
+	 
+	        Socket clientSocket = null;
+	        try {
+	            clientSocket = serverSocket.accept();
+	        } catch (IOException e) {
+	            System.err.println("Accept failed.");
+	            System.exit(1);
+	        }
+	 
+	        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+	        BufferedReader in = new BufferedReader(
+	                new InputStreamReader(
+	                clientSocket.getInputStream()));
+	        String inputLine, outputLine;
+	        KnockKnockProtocol kkp = new KnockKnockProtocol();
+	 
+	        outputLine = kkp.processInput(null);
+	        out.println(outputLine);
+	 
+	        while ((inputLine = in.readLine()) != null) {
+	             outputLine = kkp.processInput(inputLine);
+	             out.println(outputLine);
+	             if (outputLine.equals("Bye."))
+	                break;
+	        }
+	        out.close();
+	        in.close();
+	        clientSocket.close();
+	        serverSocket.close();
+	    }
 }
+

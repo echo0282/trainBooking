03d2881478545afcea722d4b1c9970d869be1f40
Mark Allen <mark.allen3@student.dit.ie>
2013-02-06 14:44:47 +0000

    Edited BookingInterface class

--- src/clientServer/BookingInterface.java
+++ src/clientServer/BookingInterface.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface BookingInterface extends Remote{
+public interface BookingInterface extends Remote {
 	
 	public void bookASeat(int route) throws RemoteException;
 	
